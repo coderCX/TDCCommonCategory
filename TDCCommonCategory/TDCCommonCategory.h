6404bc0f0be48b64e8d27074df1ab182669447d5
chengxi <chengxi@chengxideMac-mini.local>
2016-11-21 14:54:07 +0800

    Release 1.0.1.

--- TDCCommonCategory/TDCCommonCategory.h
+++ TDCCommonCategory/TDCCommonCategory.h
@@ -4,7 +4,7 @@
 //
 //  Created by chengxi on 16/11/20.
 //  Copyright © 2016年 chengxi. All rights reserved.
-//
+//   使用时只需要导入本头文件就可以了，其他头文件无需导入
 
 #ifndef TDCCommonCategory_h
 #define TDCCommonCategory_h
