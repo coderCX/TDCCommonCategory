6cf4741c08863f63447ce6b5ebb486d6b11ecf15
chengxi <chengxi@chengxideMac-mini.local>
2016-12-14 15:29:32 +0800

    Release 1.1.1

--- TDCCommonCategory/NSString+Util.m
+++ TDCCommonCategory/NSString+Util.m
@@ -38,10 +38,10 @@
     unsigned char result[16];
     CC_MD5( cStr, strlen(cStr), result );
     return [[NSString stringWithFormat:@"%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X",
-            result[0], result[1], result[2], result[3], 
-            result[4], result[5], result[6], result[7],
-            result[8], result[9], result[10], result[11],
-            result[12], result[13], result[14], result[15]] lowercaseString];
+             result[0], result[1], result[2], result[3],
+             result[4], result[5], result[6], result[7],
+             result[8], result[9], result[10], result[11],
+             result[12], result[13], result[14], result[15]] lowercaseString];
 }
 
 + (NSString *)html2Text:(NSString *)html
@@ -95,19 +95,11 @@
     if (!str || str.length == 0) {
         return contentSize;
     }
-    if (iOS7) {
-        // 获取该段attributedString的属性字典
-        NSDictionary *attribute = @{NSFontAttributeName:font};
-        // 计算文本的大小
-        contentSize = [str boundingRectWithSize:size
-                                        options:NSStringDrawingUsesLineFragmentOrigin |NSStringDrawingUsesFontLeading attributes:attribute context:nil].size;
-    }else if(IOS6){
-        NSDictionary *attrs = @{NSFontAttributeName: font};
-        NSAttributedString *attribStr = [[NSAttributedString alloc] initWithString:str attributes:attrs];
-        contentSize = [attribStr boundingRectWithSize:size options:NSStringDrawingUsesLineFragmentOrigin |NSStringDrawingUsesFontLeading context:nil].size;
-    } else {
-        contentSize = [str sizeWithFont:font constrainedToSize:size];
-    }
+    // 获取该段attributedString的属性字典
+    NSDictionary *attribute = @{NSFontAttributeName:font};
+    // 计算文本的大小
+    contentSize = [str boundingRectWithSize:size
+                                    options:NSStringDrawingUsesLineFragmentOrigin |NSStringDrawingUsesFontLeading attributes:attribute context:nil].size;
     return contentSize;
 }
 
